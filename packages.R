570b73d21bf8dba08fc0f1cdd42b3621fe60a0a4
sanjurdietrich <85748603+sanjurdietrich@users.noreply.github.com>
2024-05-13 16:05:58 -0400

    List packages required for may 13th submission

--- packages.R
+++ packages.R
@@ -1,3 +1,5 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("dplyr","data.table","tidyr"), repos="https://cran.r-project.org", dependencies=TRUE)
+install.packages(c("gtsummary", "data.table", "tidyverse", "dplyr", "purrr", "here", "naniar", "mice", "randomForest"), repos="https://cran.r-project.org", dependencies=TRUE)
+
+
