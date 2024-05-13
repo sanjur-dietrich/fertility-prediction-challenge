2df3407ba0df8c8e8c1c845188414d15575d4d74
sanjurdietrich <85748603+sanjurdietrich@users.noreply.github.com>
2024-05-13 16:06:21 -0400

    Train_save_model function for May 13 submission

--- training.R
+++ training.R
@@ -8,19 +8,20 @@
 
 train_save_model <- function(cleaned_df, outcome_df) {
   # Trains a model using the cleaned dataframe and saves the model to a file.
-
+  
   # Parameters:
   # cleaned_df (dataframe): The cleaned data from clean_df function to be used for training the model.
   # outcome_df (dataframe): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
-
+  
   ## This script contains a bare minimum working example
-  set.seed(1) # not useful here because logistic regression deterministic
+  set.seed(111) # not useful here because logistic regression deterministic
   
   # Combine cleaned_df and outcome_df
-  model_df <- merge(cleaned_df, outcome_df, by = "nomem_encr")
+  model_df <- merge(cleaned_df, outcome_df, by = "nomem_encr", all.y=TRUE)  %>% select(-c(nomem_encr))
   
-  # Logistic regression model
-  model <- glm(new_child ~ age, family = "binomial", data = model_df)
+  # Random forest model 
+  # model <- randomForest(new_child ~ ., data = model_df, ntree=1000, proximity=TRUE)
+  model <- randomForest(new_child ~ ., data = model_df, proximity=TRUE, na.action=na.roughfix)
   
   # Save the model
   saveRDS(model, "model.rds")
